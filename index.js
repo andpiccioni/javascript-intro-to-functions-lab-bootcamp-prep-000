e3cb369d3d193766e5daa832b29ff53ac7e84153
Andrea Piccioni <andpiccioni@gmail.com>
2017-10-07 17:15:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,14 +19,6 @@ function sayHiToGrandma(string){
     return "I can't hear you!"}
   else if (string.toUpperCase() === string){
     return "YES INDEED!"}
-}
-
-function sayHiToGrandma(string){
-  if (string.toUpperCase() === string)
-  return "YES INDEED!"
-}
-
-function sayHiToGrandma(string){
-  if (string === "I love you, Grandma.")
-  return "I love you, too."
+  else if (string === "I love you, Grandma."){
+    return "I love you, too."}
 }
